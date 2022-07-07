aad72e795fd2a8ad4fec3e4a5ce64f5532c79080
KrzysiekLu <krzysztof.luuczak@gmail.com>
2022-07-07 21:02:19 +0200

    first commit

---
+++ TransactionsList.js
@@ -0,0 +1,24 @@
+import Localbase from "localbase";
+import App from "./script.js";
+
+class TransactionsList {
+  dataBase = new Localbase("Wydatki");
+  transactions;
+  last;
+  constructor() {
+    this.last = document.querySelector(".last");
+    this.last.addEventListener("click", () => this.getData());
+  }
+  getData() {
+    this.dataBase
+      .collection("expances")
+      .get()
+      .then((transactions) => {
+        this.transactions = transactions;
+        console.log(this.transactions);
+      });
+  }
+  renderTransactions() {}
+}
+
+export default transactionsList = new TransactionsList();
---
+++ script.js
@@ -0,0 +1,63 @@
+import Localbase from "localbase";
+import dateModule from "./date.js";
+import TransactionsList from "./TransactionsList.js";
+
+export class App {
+  dataBase;
+  addBtn;
+  categoryInput;
+  transactionValueInput;
+
+  date = dateModule.date;
+
+  constructor() {
+    this.dataBase = new Localbase("Wydatki");
+    this.addBtn = document.querySelector(".add");
+    this.categoryInput = document.querySelector(".catagory");
+    this.transactionValueInput = document.querySelector(".value");
+    this.dateInput = document.querySelector(".date");
+
+    this.addListeners();
+  }
+
+  addListeners() {
+    this.addBtn.addEventListener("click", () => this.createExpanse());
+  }
+  clearInputs() {
+    this.categoryInput.value = "";
+    this.transactionValueInput.value = "";
+    this.dateInput.value = "";
+  }
+  createExpanse() {
+    console.log(this.date.dateEdited);
+
+    if (this.validateInputs()) {
+      const expance = new Expense(
+        this.transactionValueInput.value,
+        this.categoryInput.value,
+        this.date.dateEdited
+      );
+      this.dataBase.collection("expances").add(expance);
+      this.clearInputs();
+    } else {
+      console.log("puste");
+      return;
+    }
+  }
+  validateInputs() {
+    return this.transactionValueInput.value !== "" &&
+      this.categoryInput.value !== ""
+      ? true
+      : false;
+  }
+}
+
+class Expense {
+  constructor(value, category, date) {
+    this.value = value;
+    this.category = category;
+    this.date = date;
+  }
+}
+
+export const app = new App();
